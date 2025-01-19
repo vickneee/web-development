b8db58960cadf022865723d012aafc5c052d6465
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-01-19 18:28:24 +0200

    Initial commit

---
+++ week2/monday-morning/week2-fe-lab4/src/Footer.jsx
@@ -0,0 +1,10 @@
+// Footer.jsx
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2025 React Lab</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ week2/monday-morning/week2-fe-lab4/src/Header.jsx
@@ -0,0 +1,10 @@
+// Header.jsx
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to React Lab</h1>
+    </header>
+  );
+}
+
+export default Header;
